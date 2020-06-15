00a32d68bb0c974a545ad9d96124677199b97869
Yliasito <yliasfranckgadie@gmail.com>
2020-06-15 08:53:11 -0400

    Affichage du resultat selon le resultat du discriminant  -Probleme actuelle :      -les valeurs sont modifie depuis le code      -Pas de X0 ni X1 ni X2

--- app/src/main/java/com/example/yliasitopo/MainActivity.java
+++ app/src/main/java/com/example/yliasitopo/MainActivity.java
@@ -6,6 +6,14 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private int x = 0;
+    private int b = 1255;
+    private int a = 200;
+    private int c = 12;
+    private int discriminant = ((b*b) - (4*a*c));
+    private int resultat_zero = (-b /(2 * a));
+    private int x1 = (int) ((-b-(Math.sqrt(discriminant)))/(2*a));
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +27,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void openDialog() {
+        if (discriminant < 0)
+        {
         Example_Dialog exampleDialog = new Example_Dialog();
         exampleDialog.show(getSupportFragmentManager(), "example dialog");
-    }
-}
+        }
+        if (discriminant > 0){
+            Example_Superieur exampleDialog = new Example_Superieur();
+            exampleDialog.show(getSupportFragmentManager(), "example dialog");
+        }
+        if (discriminant == 0){
+            Example_Egale exampleDialog = new Example_Egale();
+            exampleDialog.show(getSupportFragmentManager(), "example dialog");
+        }
+}   }
